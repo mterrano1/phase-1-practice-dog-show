72bd3be85f1266e95aa8b69d1d5e98d61199860d
Christopher Terrano <mterrano1@yahoo.com>
2021-12-17 00:29:55 -0600

    commit tree 47ba529853a5635d8d755edbec4324e31900a780

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(dogs => dogs.forEach(dog => renderTable(dog)))
 })
 
+// function newFetch(){
+//     fetch('http://localhost:3000/dogs')
+//     .then(res => res.json())
+//     .then(dogs => dogs.forEach(dog => renderTable(dog)))
+// }
+
+
 function renderTable(dog){
     const tableBody = document.querySelector('#table-body');
     const tr = document.createElement('tr');
     const btn = document.createElement('button');
     btn.innerText = 'Edit Dog'
     tr.className = dog.name;
+    tr.id = dog.id;
 
     tableBody.appendChild(tr)
     tr.insertCell(0).innerText = dog.name
@@ -19,8 +27,32 @@ function renderTable(dog){
 
     btn.addEventListener('click', (e)=> {
         let selectedDog = e.target.parentElement.parentElement;
-        console.log(selectedDog)
-        // selectedDog.id = 'selectedDog'
+        const dogForm = document.querySelector('#dog-form');
+        // console.log(selectedDog.id)
+
+        dogForm.addEventListener('submit', (ev)=>{
+            ev.preventDefault();
+            let inputName = ev.target[0].value;
+            let inputBreed = ev.target[1].value;
+            let inputSex = ev.target[2].value;
+            // console.log(selectedDog.id)
+            
+            fetch(`http://localhost:3000/dogs/${selectedDog.id}`,{
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                },
+                body: JSON.stringify({
+                    name: inputName,
+                    breed: inputBreed,
+                    sex: inputSex,
+                })
+            })
+            .then(res => res.json())
+            .then(dog => renderTable(dog))
+
+        })
     })
 
 }
