ebffc5a7bc0c35f6575b0e43d91c1b807e6d5436
Christopher Terrano <mterrano1@yahoo.com>
2021-12-16 23:18:21 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    fetch('http://localhost:3000/dogs')
+    .then(res => res.json())
+    .then(dogs => dogs.forEach(dog => renderTable(dog)))
 })
+
+function renderTable(dog){
+    const tableBody = document.querySelector('#table-body');
+    const tr = document.createElement('tr');
+    const btn = document.createElement('button');
+    btn.innerText = 'Edit Dog'
+    tr.className = dog.name;
+
+    tableBody.appendChild(tr)
+    tr.insertCell(0).innerText = dog.name
+    tr.insertCell(1).innerText = dog.breed
+    tr.insertCell(2).innerText = dog.sex
+    tr.insertCell(3).appendChild(btn)
+
+    btn.addEventListener('click', (e)=> {
+        let selectedDog = e.target.parentElement.parentElement;
+        console.log(selectedDog)
+        // selectedDog.id = 'selectedDog'
+    })
+
+}
+
